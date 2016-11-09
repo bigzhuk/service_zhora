a95c08ca4e36d80e060a21012fb29dc72543607b
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-11-09 22:48:40 +0300

    добавил метрики и вакансии

--- pages/vacansy.php
+++ pages/vacansy.php
@@ -1,9 +1,39 @@
 <h1>Вакансии</h1>
 
 <div class="container">
+	<div class="vacansy_item">
+		<h3>Автослесарь</h3>
+		<div class="bordered_div">
+			<div style="padding: 5px">
+				<div><strong>График работы:</strong> 5/2, восьмичасовой рабочий день.</div>
+				<div><strong>Опыт работы:</strong> от 1 года.</div>
+				<div><strong>З/п:</strong> по результатам собеседования.</div>
+			</div>
+		</div>
+	</div>
+	<div class="vacansy_item">
+		<h3>Автослесарь-моторист</h3>
+		<div class="bordered_div">
+			<div style="padding: 5px">
+				<div><strong>График работы:</strong> 5/2, восьмичасовой рабочий день.</div>
+				<div><strong>Опыт работы:</strong> от 1 года.</div>
+				<div><strong>З/п:</strong> по результатам собеседования.</div>
+			</div>
+		</div>
+	</div>
+	<div class="vacansy_item">
+		<h3>Автоэлектрик-диагност</h3>
+		<div class="bordered_div">
+			<div style="padding: 5px">
+				<div><strong>График работы:</strong> Гибкий, по договоренности.</div>
+				<div><strong>Опыт работы:</strong> от 1 года.</div>
+				<div><strong>З/п:</strong> по результатам собеседования.</div>
+			</div>
+		</div>
+	</div>
 
 	<p>
-		В настоящее время, открытых вакансий нет. Но если вы считаете, что ваше резюме может нас заинтересовать,
+		Если вы считаете, что ваше резюме может нас заинтересовать,
 		отправляйте его на адрес: <b><?php echo App::COMPANY_EMAIL?></b> с пометкой "Работа".
 	</p>
 
