ec01798d63ae6218216eb3355f678c24256ca464
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-12-02 16:18:21 +0300

    поправил контакты 2

--- pages/contacts.php
+++ pages/contacts.php
@@ -1,8 +1,7 @@
 <h1>Контакты</h1>
 
 <div class="container">
-<p>
-	<center>
+	<div align="center">
 		<table id="contacts_table">
 			<tr>
 				<td>График работы:</td><td>8:00 до 23:00, без выходных</td>
@@ -18,12 +17,10 @@
 				<td>Электронная почта:</td><td><?= App::COMPANY_EMAIL?></td>
 			</tr>
 		</table>
-	</center>
-
-</p>
-	<center>
+		<br/>
+		<h2>Схема проезда</h2>
 		<div id="map_2" style="width: 800px; height: 720px;">
 			<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=OklSB4HEyZ5MTFykxA-WsjzVpfvKg6S0&width=800&height=720&lang=ru_RU&sourceType=constructor"></script>
 		</div>
-	</center>
+	</div>
 </div>
