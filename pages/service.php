40f1885777d56191e2bd10024b36fb885bb9b302
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-12-02 14:28:24 +0300

    поправил контакты

--- pages/service.php
+++ pages/service.php
@@ -5,23 +5,23 @@
 	Наш <strong>автосервис в Пушкино</strong> предоставляет услуги по ремонту автомобилей, такие как:
 </p>
 	<ul>
-		<li>Кузовной ремонт</li>
-		<li>Ремонт двигателя</li>
-		<li>Ремонт КПП</li>
-		<li>Ремонт электрики</li>
+		<li><a href="/service/car_repair" class="dark_gray_link">Кузовной ремонт</a></li>
+		<li><a href="/service/engine_repair" class="dark_gray_link">Ремонт двигателя</a></li>
+		<li><a href="/service/gear_repair" class="dark_gray_link">Ремонт КПП</a></li>
+		<li><a href="/service/electrics_repair" class="dark_gray_link">Ремонт электрики</a></li>
 	</ul>
 <p>
 	Также для вас работают:
 </p>
 	<ul>
-		<li>Шиномонтаж</li>
-		<li>Эвакуатор</li>
-		<li>Страховой агент</li>
+		<li><a href="/service/tyres_service" class="dark_gray_link">Шиномонтаж</a></li>
+		<li><a href="/service/evacuator" class="dark_gray_link">Эвакуатор</a></li>
+		<li><a href="/service/insurance" class="dark_gray_link">Страховой агент</a></li>
 	</ul>
 <p>
 	Обращаем внимание на нашу эксклюзивную для Пушкинского района услугу:
 </p>
 	<ul>
-		<li>Ремонт грузовых авто</li>
+		<li><a href="/service/truck_repair" class="dark_gray_link">Ремонт грузовых авто</a></li>
 	</ul>
 </div>
