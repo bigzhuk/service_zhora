6a131f3009b7c43344bd790616ae5c60cb6bbcc2
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-12-12 17:43:08 +0300

    добавил микроразметку

--- pages/contacts.php
+++ pages/contacts.php
@@ -1,39 +1,62 @@
 <h1>Контакты</h1>
 
 <div class="container">
-	<div align="center">
-		<table id="contacts_table">
-			<tr>
-				<td>График работы:</td><td>8:00 до 23:00, без выходных</td>
-			</tr>
-			<tr>
-				<td>Телефон:</td><td><?= App::$phones[0].', '.App::$phones[1];?></td>
-			</tr>
-			<tr>
-				<td>Адрес:</td><td>141207, МО, г. Пушкино, ул. Лермонтова, 37-а («Росгеология»)</td>
-			</tr>
-			<tr>
-				<td>Электронная почта:</td><td><?= App::COMPANY_EMAIL?></td>
-			</tr>
-			<tr>
-				<td>Группа в VK:</td><td><a href="https://vk.com/public131730975" target="_blank">https://vk.com/public131730975</a></td>
-			</tr>
-			<tr>
-				<td>Реквизиты:</td>
-				<td>
+	<article itemscope itemtype="http://schema.org/Article" id="main_content">
+	<div itemscope itemtype="http://schema.org/Organization" align="center">
+		<div class="table">
+			<div class="row">
+				<div class="cell left_light_gray_border top_light_gray_border">Наименование организации:</div>
+				<div class="cell both_horizontal_light_gray_border top_light_gray_border"><span itemprop="name">ООО «Автомоторс-50»<span itemprop="name"></div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border top_light_gray_border">Адрес:</div>
+				<div class="cell both_horizontal_light_gray_border top_light_gray_border">
+					<span itemprop="postalCode">141207</span>,
+					<span itemprop="addressLocality">Россиия, МО, г. Пушкино</span>,
+					<span itemprop="streetAddress">ул. Лермонтова, д. 37-а</span>(«Росгеология»)
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border both_vertical_light_gray_border">Телефон:</div>
+				<div class="cell both_horizontal_light_gray_border both_vertical_light_gray_border">
+					<?= App::$phones[0]?>,
+					<?= App::$phones[1]?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border both_vertical_light_gray_border">График работы:</div>
+				<div class="cell both_horizontal_light_gray_border both_vertical_light_gray_border">
+					8:00 до 23:00, без выходны
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border both_vertical_light_gray_border">Электронная почта:</div>
+				<div class="cell both_horizontal_light_gray_border both_vertical_light_gray_border">
+					<?= App::COMPANY_EMAIL ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border both_vertical_light_gray_border">Группа в VK:</div>
+				<div class="cell both_horizontal_light_gray_border both_vertical_light_gray_border">
+					<a href="https://vk.com/public131730975" target="_blank">https://vk.com/public131730975</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="cell left_light_gray_border both_vertical_light_gray_border">Реквизиты:</div>
+				<div class="cell both_horizontal_light_gray_border both_vertical_light_gray_border">
 					ИНН: 50381004488<br/>
 					КПП: 503801001<br/>
 					Банк: АКБ «АБСОЛЮТ БАНК» (ПАО)<br/>
 					Р/с: 40702810324000002645 <br/>
 					К/с: 30101810500000000976 <br/>
 					БИК: 044525976 <br/>
-				</td>
-			</tr>
-		</table>
-		<br/>
+				</div>
+			</div>
+		</div>
 		<h2>Схема проезда</h2>
 		<div id="map_2" style="width: 800px; height: 720px;">
 			<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=OklSB4HEyZ5MTFykxA-WsjzVpfvKg6S0&width=800&height=720&lang=ru_RU&sourceType=constructor"></script>
 		</div>
 	</div>
+	</article>
 </div>
